4a1943e124e1a50cff4cc067c6f17bd587106786
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-02 09:58:19 +0100

    Update tourController.js

---
+++ controllers/tourController.js
@@ -0,0 +1,140 @@
+const Tour = require('./../model/tourModel');
+const APIFeatures = require('./../utils/apiFeatures')
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
+exports.getAllTours = async (req, res) => {
+  try {
+    
+    // *EXECUTE QUERY* //
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+      const tours = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      result: tours.length,
+      data: {
+        tours
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+    });
+    console.log(err);
+  }
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id); // id come from the tourRoutes router.route('/:id')
+    // Tour.findOne({_id: req.params.id})
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent! 💢',
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'seccuss',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'seccuss',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+        {
+          $match: { ratingsAverage: { $gte: 4.5 } }
+        },
+        {
+          $group: {
+            _id: null,
+            avgRating: { $avg: '$ratingsAverage' },
+            avgPrice: { $avg: '$price' },
+            minPrice: { $min: '$price' },
+            maxPrice: { $max: '$price' }
+          }
+        }
+    ])
+    
+    res.status(200).json({
+      status: 'seccuss',
+      data: {
+        stats
+      }
+    });
+
+  } catch(err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    })
+  }
+}
