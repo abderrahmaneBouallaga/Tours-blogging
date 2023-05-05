200fed23bb03d5814a7750da192ff2d259b7a275
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-05 17:24:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
 
-const AppError = require('./utils/appError')
-const globalErrHandler = require('./controllers/errController')
-const app = express();
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+const app = express();
 
-// *Middleware* //
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-// *Routes* //
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
-})
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-app.use(globalErrHandler)
-
+app.use(globalErrorHandler);
 
 module.exports = app;
-
-
