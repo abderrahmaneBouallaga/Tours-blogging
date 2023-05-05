a51eb5ba9aeb9baca00617dfae06b989e88a015c
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-05 17:24:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -13,19 +19,20 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
     useFindAndModify: false,
+    useUnifiedTopology: true
   })
-  .then(() => {
-    console.log('DB connection successful!');
-  })
-  .catch((err) => console.log(err));
-
-
-
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
 
-app.listen(port, () => {
-  console.log(`app running on port ${port} ✅`);
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
