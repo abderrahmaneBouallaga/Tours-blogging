63570448fa6ecf2c80cbab0c5a9a1b8ce390db1d
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-07 16:31:31 +0100

    Create userModel.js

---
+++ model/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please tell us your name!']
+        },
+        email: {
+            type: String,
+            required: [true, 'Please provide your email'],
+            uniquer: true,
+            lowercase: true,
+            validate: [validator.isEmail, 'Please provide a valid email']
+        },
+        photo: String,
+        password:{
+            type: String,
+            required: [true, 'Please provide a password'],
+            minlength: 8
+        },
+        passwrodConfirm: {
+            type: String,
+            required: [true, 'Please confirm password']
+        }
+    }
+)
+
+const User = mongoose.model('User', userSchema)
+
+
+module.exports = User;
