4ef242513d1aaa881acc466a3430244ee333040b
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-08 18:41:49 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,9 +8,12 @@ const {
 } = require('../controllers/userController')
 const authController = require('../controllers/authController')
 
+
+
 const router = express.Router();
 
 router.post('/signup', authController.signup)
+router.post('/login', authController.login)
 
 router.route('/').get(getAllUsers).post(createUser);
 router
