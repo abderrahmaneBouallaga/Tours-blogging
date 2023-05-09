57e341d8bc702bb3421b976b1ef3154a4a6a1436
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-09 10:28:19 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../model/userModel');
 const jwt = require('jsonwebtoken');
@@ -15,9 +16,10 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt
   });
 
-  const token = signToken(newUser._id)
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -43,9 +45,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
   // If everything ok, send token to client
 
-  console.log(user);
-
-  const token = signToken(user._id)
+  const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token,
@@ -53,21 +53,34 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
-    let token;
-    // Getting token and check of it's there
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        token = req.headers.authorization.split(' ')[1];
-    }
-    console.log(token)
+  let token;
+  // Getting token and check of it's there
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
 
-    if(!token) {
-        return next(new AppError('Your are not logged in! Please log in to get access.', 401))
-    }
-    // Verification token
 
-    // Check if user still exists
+  if (!token) {
+    return next(
+      new AppError('Your are not logged in! Please log in to get access.', 401)
+    );
+  }
+  // Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+  // Check if user still exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(new AppError('The user belonging to this token does no longer exist.', 401))
+  }
+  // Check if user changed password after the token was issued
+  if(freshUser.changedPasswordAfter(decoded.iat)){
+    return next(new AppError('User recently changed password! Please log in again', 401))
+  }
 
-    // Check if user changed password after the token was issued
-
-    next()
-})
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = freshUser;
+  next();
+});
