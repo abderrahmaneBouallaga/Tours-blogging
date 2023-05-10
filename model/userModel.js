f7869e8b873b6f039993b0d0145e66b158efef13
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-10 16:11:20 +0100

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
@@ -39,7 +40,9 @@ const userSchema = new mongoose.Schema(
         },
         passwordChangedAt: {
             type: Date
-        }
+        },
+        passwordResetToken: String,
+        passwordResetExpires: Date
     }
 )
 
@@ -50,6 +53,13 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, 12);
     
     this.passwordConfirm = undefined;
+    next();
+})
+
+userSchema.pre('save', function(next){
+    if(!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
     next();
 })
 
@@ -65,6 +75,13 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
     }
 
     return false;
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    return resetToken;
 }
 
 const User = mongoose.model('User', userSchema)
