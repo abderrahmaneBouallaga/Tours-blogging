a896383a7dec393b932aff482a5ff947bba0a273
abderrahmaneBouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-05-14 15:55:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes')
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean')
 const hpp = require('hpp')
@@ -18,7 +19,7 @@ app.use(helmet())
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-// Limit requests from same API
+// Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -50,6 +51,7 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter)
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
